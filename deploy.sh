5342e981ef9b24c06ed55023bccc7af9f2698de2
re102162189 <re102162189@gmail.com>
2021-01-22 11:17:36 +0800

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 cd springio-api
 mvn clean package -DskipTests=true 
-docker build -t springio-api .
-docker tag springio-api re102162189/springio-api:1.0.0
-docker push re102162189/springio-api:1.0.0 
+docker build -t springio-demo .
+docker tag springio-demo re102162189/springio-demo:1.0.0
+docker push re102162189/springio-demo:1.0.0 
 
 cd ..
-kubectl create namespace springio
 
 kubectl apply -f base
 kubectl apply -f mysql  
